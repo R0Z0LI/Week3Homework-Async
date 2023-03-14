da5cf304f7d4e355deace734c6a80bd5f88f1053
Rohringer Zoltan <rohringer.zoltan@gmail.com>
2023-03-14 09:57:24 +0100

    feat: fetch data

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,15 @@
-fetch;
+let data;
+
+const fetchProfileInfo = async () => {
+  const res = await fetch("https://my.api.mockaroo.com/profiles.json", {
+    method: "GET",
+    headers: {
+      "X-API-Key": "fdf950f0",
+    },
+  });
+  data = await res.json();
+};
+
+fetchProfileInfo();
+
+console.log(data);
